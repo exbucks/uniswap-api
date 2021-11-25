3da820420da68fa793409a13fa5bfab7ae90639f
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-26 04:45:51 +0900

    update swap funcs

--- swap/swap.go
+++ swap/swap.go
@@ -1,11 +1,14 @@
 package swap
 
 import (
+	"fmt"
 	"strconv"
+	"time"
 
 	uniswap "github.com/hirokimoto/uniswap-api"
 )
 
+// SwapName returns token name in a swap.
 func SwapName(swap uniswap.Swap) (name string) {
 	if swap.Pair.Token0.Symbol == "WETH" {
 		name = swap.Pair.Token1.Name
@@ -15,6 +18,7 @@ func SwapName(swap uniswap.Swap) (name string) {
 	return name
 }
 
+// SwapPrice returns token price of in a swap.
 func SwapPrice(swap uniswap.Swap) (price float64, target string) {
 	amountUSD, _ := strconv.ParseFloat(swap.AmountUSD, 32)
 	amountToken, _ := strconv.ParseFloat(swap.Amount0Out, 32)
@@ -39,4 +43,23 @@ func SwapPrice(swap uniswap.Swap) (price float64, target string) {
 
 	price = amountUSD / amountToken
 	return price, target
+}
+
+// SwapOld returns how many hours have been passed from the latest swap.
+func SwapOld(swap uniswap.Swap) (float64, string) {
+	timestamp, _ := strconv.ParseInt(swap.Timestamp, 10, 64)
+	unixTimestamp := time.Unix(timestamp, 0)
+	now := time.Now()
+	old := now.Sub(unixTimestamp)
+	olds := ""
+	if old.Minutes() <= 1 {
+		olds = fmt.Sprintf("%f", old.Seconds()) + "seconds"
+	}
+	if old.Minutes() > 1 && old.Hours() < 1 {
+		olds = fmt.Sprintf("%f", old.Minutes()) + "minutes"
+	}
+	if old.Hours() >= 1 {
+		olds = fmt.Sprintf("%f", old.Hours()) + "hours"
+	}
+	return old.Hours(), olds
 }
