b321eea0b568074faf64858d5f8c1ccf806384a7
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-26 04:34:50 +0900

    add swap package

---
+++ swap/swap.go
@@ -0,0 +1,33 @@
+package swap
+
+import (
+	"strconv"
+
+	uniswap "github.com/hirokimoto/uniswap-api"
+)
+
+func SwapPrice(swap uniswap.Swap) (price float64, target string) {
+	amountUSD, _ := strconv.ParseFloat(swap.AmountUSD, 32)
+	amountToken, _ := strconv.ParseFloat(swap.Amount0Out, 32)
+
+	if swap.Pair.Token0.Symbol == "WETH" {
+		if swap.Amount0In != "0" && swap.Amount1Out != "0" {
+			amountToken, _ = strconv.ParseFloat(swap.Amount1Out, 32)
+			target = "BUY"
+		} else if swap.Amount0Out != "0" && swap.Amount1In != "0" {
+			amountToken, _ = strconv.ParseFloat(swap.Amount1In, 32)
+			target = "SELL"
+		}
+	} else {
+		if swap.Amount0Out != "0" && swap.Amount1In != "0" {
+			amountToken, _ = strconv.ParseFloat(swap.Amount0Out, 32)
+			target = "BUY"
+		} else if swap.Amount0In != "0" && swap.Amount1Out != "0" {
+			amountToken, _ = strconv.ParseFloat(swap.Amount0In, 32)
+			target = "SELL"
+		}
+	}
+
+	price = amountUSD / amountToken
+	return price, target
+}
