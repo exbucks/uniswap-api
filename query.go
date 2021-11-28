2ed0d69cc55a50b1df68d6e34041397327646b62
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-28 22:20:20 +0900

    fix request query

--- query.go
+++ query.go
@@ -12,16 +12,16 @@ import (
 func request(query map[string]string, target chan string) {
 	jsonQuery, _ := json.Marshal(query)
 	request, _ := http.NewRequest("POST", "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2", bytes.NewBuffer(jsonQuery))
-	client := &http.Client{Timeout: time.Second * 50}
+	client := &http.Client{Timeout: time.Second * 10}
 	response, err := client.Do(request)
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 		target <- ""
-		return
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		target <- string(data)
 	}
 	defer response.Body.Close()
-	data, _ := ioutil.ReadAll(response.Body)
-	target <- string(data)
 }
 
 func QueryBundles() map[string]string {
