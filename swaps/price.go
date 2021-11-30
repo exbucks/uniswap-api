2f8f067f2619929f7bf7446f3028c30b245e6bc7
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-30 12:32:47 +0900

    add more func

--- swaps/price.go
+++ swaps/price.go
@@ -37,3 +37,13 @@ func LastPriceChanges(swaps uniswap.Swaps) (float64, float64, float64) {
 	}
 	return 0.0, 0.0, 0.0
 }
+
+// Average returns the average price of swaps
+func Average(swaps []uniswap.Swap) float64 {
+	sum := 0.0
+	for _, item := range swaps {
+		price, _ := swap.Price(item)
+		sum += price
+	}
+	return sum / float64(len(swaps))
+}
